52651325d1b1af9a4ca2f851c01fb2def4bbd01d
Madhu Rakhal Magar <madhurakhalmagar@Madhus-MacBook-Pro.local>
2026-07-16 17:12:38 +0200

    update

--- projects/multi-level-menu/src/public-api.ts
+++ projects/multi-level-menu/src/public-api.ts
@@ -3,7 +3,7 @@ import { MenuItemTemplateDirective } from "./lib/directives/menu-item.template.d
 import { MultiLevelMenu } from "./lib/multi-level-menu";
 
 /*
- * Public API Surface of multi-level-menu
+ * Public API Surface of @madhurakhalmagar/ngx-folder-tree
  */
 export { MultiLevelMenu, FolderTemplateDirective , MenuItemTemplateDirective } 
 export type { SidebarMenuFolder, SidebarMenuItem } from "./lib/interfaces/sidebar-menu.interface";
